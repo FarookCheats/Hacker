b4af55a9acd804b4352735692ddc17c43e03ec8d
Farook Cheats <89658300+FarookCheats@users.noreply.github.com>
2021-10-03 12:38:47 +0700

    Add files via upload

---
+++ abth.py
@@ -0,0 +1,32 @@
+import requests,json,time,threading,os,sys
+import urllib.request, os, threading, time, random, sys
+import colorama
+from colorama import Fore, Style, init
+#from requests_futures.sessions import FuturesSession
+#from requests_futures import sessions
+os.system("clear")
+print("")
+print(Fore.GREEN + "  ===Menu Hacker===")
+print("")
+print(Fore.BLUE + "[1] Facebook")
+print(Fore.BLUE + "[2] Free Fire")
+print(Fore.BLUE + "[3] Roblox")
+print(Fore.BLUE + "[4] Google")
+print(Fore.BLUE + "[5] Youtube")
+print("")
+verfly = input(Fore.YELLOW + "Numbers : ")
+if verfly == '1':
+	os.system("python facebook.py")
+	
+if verfly == '2':
+	os.system("python ff.py")
+	
+if verfly == '3':
+	os.system("python Roblox.py")
+	
+if verfly == '4':
+	os.system("python Google.py")
+	
+if verfly == '5':
+	os.system("python Youtube.py")
+	

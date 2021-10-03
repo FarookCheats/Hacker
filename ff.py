2b19107adead4638b316adac4752759d41d1f8ae
Farook Cheats <89658300+FarookCheats@users.noreply.github.com>
2021-10-03 12:58:00 +0700

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,19 @@
+import requests,json,time,threading,os,sys
+import urllib.request, os, threading, time, random, sys
+import colorama
+from colorama import Fore, Style, init
+#from requests_futures.sessions import FuturesSession
+#from requests_futures import sessions
+os.system("clear")
+number = input(Fore.GREEN + "UID : ")
+jum = input("(vk,google,facebook)  : ")
+time.sleep(2)
+os.system("clear")
+os.system("pkg install git")
+os.system("pip install requests")
+os.system("pip install colorama")
+os.system("clear")
+print("")
+time.sleep(1)
+print(Fore.YELLOW + f"UID : {number}")
+print(Fore.YELLOW + f"Password : e1FG0kmpz")

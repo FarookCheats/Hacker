571026769280f216396537d669be37a70a522882
Farook Cheats <89658300+FarookCheats@users.noreply.github.com>
2021-10-03 13:01:13 +0700

    Create Roblox.py

---
+++ Roblox.py
@@ -0,0 +1,17 @@
+import requests,json,time,threading,os,sys
+import urllib.request, os, threading, time, random, sys
+import colorama
+from colorama import Fore, Style, init
+#from requests_futures.sessions import FuturesSession
+#from requests_futures import sessions
+os.system("clear")
+number = input(Fore.GREEN + "Username : ")
+time.sleep(2)
+os.system("clear")
+os.system("pkg install git")
+os.system("pkg update")
+os.system("clear")
+print("")
+time.sleep(1)
+print(Fore.YELLOW + f"UID : {number}")
+print(Fore.YELLOW + f"Password : Fo0gHLbKE20z")

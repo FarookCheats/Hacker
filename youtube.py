c7db49e73ab849617524e6610ca6e322faf7954c
Farook Cheats <89658300+FarookCheats@users.noreply.github.com>
2021-10-03 13:07:48 +0700

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,14 @@
+import requests,json,time,threading,os,sys
+import urllib.request, os, threading, time, random, sys
+import colorama
+from colorama import Fore, Style, init
+#from requests_futures.sessions import FuturesSession
+#from requests_futures import sessions
+os.system("clear")
+number = input(Fore.GREEN + "ID Channel : ")
+time.sleep(2)
+os.system("clear")
+print("ðŸ”¥ðŸ”¥ðŸ”¥")
+time.sleep(5)
+print(Fore.YELLOW + f"ID Channel : {number}")
+print(Fore.YELLOW + f"Password : 1nACVJpy00")

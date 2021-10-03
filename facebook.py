d3e2790c07203ba99b2244592980fc4e5ad0850b
Farook Cheats <89658300+FarookCheats@users.noreply.github.com>
2021-10-03 12:49:20 +0700

    Create facebook.py

---
+++ facebook.py
@@ -0,0 +1,20 @@
+import requests,json,time,threading,os,sys
+import urllib.request, os, threading, time, random, sys
+import colorama
+from colorama import Fore, Style, init
+#from requests_futures.sessions import FuturesSession
+#from requests_futures import sessions
+number = input(Fore.RED + "ID : ")
+time.sleep(1)
+print("")
+print(Fore.YELLOW + "Start [66]")
+verfly = input("Numbers : ")
+print("")
+time.sleep(1)
+print(Fore.GREEN + "Loads..")
+time.sleep(3)
+os.system("clear")
+if verfly == '66':
+	print(f"Username : {number}")
+	print("Password : e8FV4fKV2o")
+	
